5afe4c43d0e1950abbf2a0e0219982f7d043d59d
Gaurav Sharma <gaurav2728@gmail.com>
2018-12-06 09:07:07 +0530

    Add validations and associations

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,2 +1,8 @@
 class Reader < ApplicationRecord
+  belongs_to :thermostat
+
+  validates :number, presence: true, numericality: { only_integer: true }
+  validates :temperature, presence: true, numericality: { only_float: true }
+  validates :humidity, presence: true, numericality: { only_float: true }
+  validates :battery_charge, presence: true, numericality: { only_float: true }
 end
--- app/models/thermostat.rb
+++ app/models/thermostat.rb
@@ -1,2 +1,6 @@
 class Thermostat < ApplicationRecord
+  has_many :readers, dependent: :destroy
+
+  validates :household_token, presence: true, uniqueness: true
+  validates :location, presence: true
 end
