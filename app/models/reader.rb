e6cf38b276a6aede9a47d23172c807805904219d
Gaurav Sharma <gaurav2728@gmail.com>
2018-12-31 10:41:07 +0530

    Generate data for development

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -5,4 +5,8 @@ class Reader < ApplicationRecord
   validates :temperature, presence: true, numericality: { only_float: true }
   validates :humidity, presence: true, numericality: { only_float: true }
   validates :battery_charge, presence: true, numericality: { only_float: true }
+
+  def self.next_number
+    Reader.connection.select_value("Select nextval('readers_id_seq')")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+MAX_RECORDS = 1..5
+
+ActiveRecord::Base.transaction do
+  # Create Thermostat
+  MAX_RECORDS.each do
+    Thermostat.create!(
+      household_token: SecureRandom.uuid,
+             location: Faker::Address.unique.city,
+    )
+  end
+
+  # Create Readers
+  thermostats = Thermostat.all
+  thermostats.each do |thermostat|
+    reader = thermostat.readers.build(
+              number: Reader.next_number,
+         temperature: Faker::Number.decimal(2),
+            humidity: Faker::Number.decimal(2),
+      battery_charge: Faker::Number.decimal(2)
+    )
+    reader.save!
+  end
+end
