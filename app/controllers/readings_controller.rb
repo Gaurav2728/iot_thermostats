7cf16096f21ee771d1b63e30f072d70a1f58d4d5
Gaurav Sharma <gaurav2728@gmail.com>
2019-02-02 10:41:07 +0530

    Setup redis and create Reader

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,10 +1,45 @@
 class ReadingsController < ApplicationController
+  before_action :fetch_thermostat
+  before_action :validate_presence, only: :create
+
   def index
   end
 
   def create
+    $redis.set(@number, params)
+    ReadingWorker.perform_async(@number, @thermostat.id, @temperature, @humidity, @battery_charge)
+    render json: {number: @number}
   end
 
   def show
   end
+
+  def stats
+  end
+
+  private
+    def fetch_thermostat
+      token = params[:household_token]
+      render json: { message: 'Please provide household token.' }, status: 401 and return if !token
+      @thermostat = Thermostat.find_by(household_token: token)
+      render json: { message: 'Household token is invalid !' }, status: 401 and return if !@thermostat
+    end
+
+    def validate_presence
+      if %w(temperature humidity battery_charge).all? {|key| params[key].present?}
+        @temperature = params[:temperature]
+        @humidity = params[:humidity]
+        @battery_charge = params[:battery_charge]
+        @number = Reader.next_number
+        reader = Reader.new(permit_params.merge!(thermostat_id: @thermostat.id, number: @number))
+        render json: { errors: reader.errors } and return if reader.invalid?
+      else
+        render json: { message: 'Please check paramaters and values should be present.' }, status: 400
+      end
+    end
+
+
+    def permit_params
+      params.permit(:temperature, :humidity, :battery_charge)
+    end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -6,7 +6,13 @@ class Reader < ApplicationRecord
   validates :humidity, presence: true, numericality: { only_float: true }
   validates :battery_charge, presence: true, numericality: { only_float: true }
 
+  after_create :clear_redis
+
   def self.next_number
     Reader.connection.select_value("Select nextval('readers_id_seq')")
+  end
+
+  def clear_redis
+    $redis.del self.number
   end
 end
